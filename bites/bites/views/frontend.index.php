c3357ea052d4e0fb07004474b5a53afd8dbb3a45
Luke Rotherfield <l.rotherfield@gmail.com>
2011-06-25 12:43:23 +0100

    Removed images and updated home page

--- bites/bites/views/frontend.index.php
+++ bites/bites/views/frontend.index.php
@@ -1,4 +1,14 @@
 <header>
-  <h1>A brand new Nibble productions application, welcome.</h1>
+  <h1>Guildford Garage, MOT and Service specialists</h1>
 </header>
-<p>This is the front end of the application, from here we can view the contents of the plugins that are installed.  Peace out homies.</p>
+<p>
+  Welcome to the home page of the Guildford Garage run by Simon Hopcroft-lopez.
+  We offer honest mechanical trade for all motor vehicles.
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a enim augue, vitae pretium diam. Sed mollis odio vel mi fringilla ultricies nec ut turpis. Proin viverra, neque in molestie interdum, augue risus egestas magna, vel eleifend eros turpis non ligula. Suspendisse id urna non velit scelerisque auctor. Curabitur faucibus pellentesque sem, sit amet dignissim purus consectetur a. Aliquam laoreet metus id urna imperdiet gravida tempus nulla varius. Nam venenatis placerat tempor. Integer lobortis, sapien laoreet mattis venenatis, lacus ante commodo ipsum, sit amet aliquam magna orci eget mi. Vivamus vitae eros sit amet ante mollis ornare. Etiam sit amet neque turpis, ac lobortis dui.
+</p>
+<p>
+Fusce consectetur lectus vitae magna placerat aliquet. Ut luctus orci ac dui porta nec viverra mi hendrerit. Proin faucibus varius eleifend. Sed dictum posuere placerat. Vestibulum id iaculis arcu. Praesent lacus enim, iaculis ac posuere eget, pharetra in odio. Nulla volutpat ante quis justo tristique sodales. Curabitur volutpat ligula ut libero porttitor rhoncus. Sed non libero mi. Cras ultrices, lectus ac lobortis viverra, leo tortor iaculis ligula, vitae pellentesque ipsum erat id augue. Integer ipsum sem, interdum sed volutpat nec, porta at ipsum. Nulla laoreet, orci quis convallis ultricies, diam nibh convallis nisi, ut dignissim turpis nulla et erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elit est, molestie porta viverra a, mattis sed massa. Fusce a arcu erat, ut suscipit sem. Ut eget fermentum odio. Aenean aliquet accumsan lectus, et mollis diam interdum a.
+</p>
+<p>
+Fusce elementum vestibulum orci, vulputate dapibus justo pharetra eget. Donec id urna viverra metus elementum pretium. Fusce volutpat luctus velit, at viverra dolor tincidunt sit amet. Fusce et fringilla magna. Nullam rhoncus hendrerit scelerisque. Morbi enim nibh, blandit ac tristique id, sollicitudin luctus lorem. Mauris justo magna, tincidunt eget rutrum id, iaculis vel enim. Integer ornare diam ut ante sagittis vitae cursus nibh elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus facilisis adipiscing nibh ac gravida.
+</p>
