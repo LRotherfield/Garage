cf18d4b09905ff3e806f1814cff7dcc78ff54f7a
Luke Rotherfield <l.rotherfield@gmail.com>
2011-06-25 16:38:09 +0100

    New font and more orange

--- templates/frontend_layout.php
+++ templates/frontend_layout.php
@@ -4,8 +4,7 @@
     <title><?= $page_title ?></title>
     <?= $this->renderMeta() ?>
     <meta name="google-site-verification" content="Uvdp0pOSmJhWcoMqL_AegxnViphnU3GSL2xl0eTBzok" />
-    <link href='http://fonts.googleapis.com/css?family=Molengo&amp;subset=latin' rel='stylesheet' type='text/css' />
-    <link href='http://fonts.googleapis.com/css?family=Reenie+Beanie&amp;subset=latin' rel='stylesheet' type='text/css' />
+    <link href='http://fonts.googleapis.com/css?family=Nunito&v1' rel='stylesheet' type='text/css'>
     <?= $this->renderScripts() ?>
     <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
