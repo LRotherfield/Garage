84b8ca61c834b334ed43a3d9326c2fe97116331d
Luke Rotherfield <l.rotherfield@gmail.com>
2011-06-26 08:27:59 +0100

    Contact form css

--- bites/bites/forms/contact.php
+++ bites/bites/forms/contact.php
@@ -1,5 +1,5 @@
 <?php
-$form = NibbleForm::getInstance('/admin', 'Submit me', 'post', true, 'flash', 'list');
+$form = NibbleForm::getInstance('', 'Send us your message', 'post', true, 'flash', 'list');
 $form->name = new Text('Please enter your name so we know who to reply to');
 $form->email = new email('Please enter your email', 6, true, false);
 $form->email->addConfirmation('Please confirm your email');
