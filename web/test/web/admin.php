edc3528ad662744cd1b1cf1746e337c45800dd9b
Luke Rotherfield <l.rotherfield@gmail.com>
2011-06-28 20:30:34 +0100

    added a change to admin for root

--- web/test/web/admin.php
+++ web/test/web/admin.php
@@ -4,7 +4,7 @@
  * Define application
  */
 define('APP', 'admin');
-include dirname(dirname(__FILE__)) . '/lib/config/config.php';
+include dirname(dirname(dirname(dirname(__FILE__)))) . '/lib/config/config.php';
 if (DBUSER == '')
   require ROOT . '/lib/config/setup.php';
 else
